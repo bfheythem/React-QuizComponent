4c8f8d9b9828aa8825a76b568a9dd7284c81b9b0
Heythem Ben Fadhel <HBenFadhel@se.linedata.com>
2020-03-02 13:58:24 +0100

    Completed module2

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
+import QuizQuestion from './QuizQuestion'
 
 let quizData = require('./quiz_data.json')
-
 class Quiz extends Component{
 
 constructor(props){
@@ -11,7 +11,9 @@ this.state={quiz_position: 1}
 render (){
     return (
         <div>
-            <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+            <div className="QuizQuestion">
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}></QuizQuestion>
+                </div>
 
     </div>
     )
